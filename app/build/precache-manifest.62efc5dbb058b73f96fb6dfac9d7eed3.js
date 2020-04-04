86d02bd95cc4aea34c01fa9e8e720fa19001d6f1
Houcem <houcem.naffati@resocom.com>
2020-04-04 21:05:33 +0200

    add  build folder

---
+++ app/build/precache-manifest.62efc5dbb058b73f96fb6dfac9d7eed3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcd086cdacf14ca95385a39bf106dd5a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f051a6ab703d5e89ae6",
+    "url": "/static/css/main.f78e1915.chunk.css"
+  },
+  {
+    "revision": "7514707ef9e3fe36e5a1",
+    "url": "/static/js/2.8bc1d8ae.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8bc1d8ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f051a6ab703d5e89ae6",
+    "url": "/static/js/main.470962fd.chunk.js"
+  },
+  {
+    "revision": "6a3ba521a668b4faa410",
+    "url": "/static/js/runtime-main.248e62a3.js"
+  }
+]);
